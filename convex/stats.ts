bf9ab64ae725540254ac54fefcd23bda64ae7086
yanqi.zong <yzong@turo.com>
2025-05-01 15:43:18 -0700

    fix: fix oss

--- convex/stats.ts
+++ convex/stats.ts
@@ -2,7 +2,7 @@ import { OssStats } from '@erquhart/convex-oss-stats'
 import { components } from './_generated/api'
 
 export const ossStats = new OssStats(components.ossStats, {
-  githubOwners: ['tanstack-dev'],
+  githubOwners: ['tanstack'],
   npmOrgs: ['tanstack', 'tannerlinsley'],
 })
 
